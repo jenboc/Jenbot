0e58c3a7a8d6468621369ae54b7df8cae2b1c28b
jenboc <47900748+jenboc@users.noreply.github.com>
2023-06-25 17:33:38 +0100

    Multipage Embed
    Taking advantage of interaction system

--- Jenbot/Interactions/Handler.cs
+++ Jenbot/Interactions/Handler.cs
@@ -6,13 +6,11 @@ namespace Jenbot.Interactions;
 public abstract class Handler
 {
     public DateTime TimeCreated { get; }
-    protected string CustomId { get; }
     protected MessageComponent MsgComponent { get; set; }
 
     protected Handler()
     {
         TimeCreated = DateTime.Now; 
-        CustomId = Guid.NewGuid().ToString();
         MsgComponent = new ComponentBuilder().Build();
     }
 
---
+++ Jenbot/Interactions/IMessageInteractable.cs
@@ -0,0 +1,9 @@
+﻿using Discord.WebSocket;
+
+namespace Jenbot.Interactions;
+
+public interface IMessageInteractable
+{
+    public Task Reply(SocketInteraction interaction);
+    public Task Send(SocketTextChannel channel);
+}
