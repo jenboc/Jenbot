af5c11d20a75dde6f156443c39c947623129ef20
jenboc <47900748+jenboc@users.noreply.github.com>
2023-10-01 19:55:22 +0100

    Create Virtual Card link

--- Jenbot/Modules/FunCommandModule.cs
+++ Jenbot/Modules/FunCommandModule.cs
@@ -1,12 +1,17 @@
-﻿using Discord.Interactions;
+﻿using System.Text;
+using Discord.Interactions;
 using Jenbot.Interactables;
 using Jenbot.Trivia;
+using ZXing;
+using ZXing.Common;
 
 namespace Jenbot.Modules;
 
 public class FunCommandModule : InteractionModuleBase<SocketInteractionContext>
 {
     private static readonly TriviaApi _triviaApi = new();
+
+    private const string VIRTUAL_CARD_URL = "http://www.jensoncain.co.uk/virtual-card";
     
     [SlashCommand("die-roll", "Roll a dice")]
     public async Task DieRoll(int numSides = 4)
@@ -31,5 +36,33 @@ public class FunCommandModule : InteractionModuleBase<SocketInteractionContext>
         var embed = new TriviaEmbed(q);
         InteractableManager.AddHandler(embed);
         await embed.Followup(Context.Interaction);
+    }
+
+    private string GetVirtualCardUrl(string header, string message)
+    {
+        if (string.IsNullOrEmpty(header) || string.IsNullOrEmpty(message))
+        {
+            return ""; 
+        }
+
+        var encodedHeader = Convert.ToBase64String(Encoding.UTF8.GetBytes(header));
+        var encodedMessage = Convert.ToBase64String(Encoding.UTF8.GetBytes(message));
+        return $"{VIRTUAL_CARD_URL}/card.html?h={encodedHeader}&m={encodedMessage}";
+    }
+
+    [SlashCommand("virtual-card-link", "Write a Virtual Card using my website!")]
+    public async Task VirtualCardLink(string header = "", string message = "")
+    {
+        await DeferAsync();
+        var url = GetVirtualCardUrl(header, message);
+
+        if (string.IsNullOrEmpty(url))
+        {
+            await FollowupAsync($"{Context.User.Mention}, either provide a header and a message or use " +
+                                $"{VIRTUAL_CARD_URL} to create a virtual card");
+            return;
+        }
+
+        await FollowupAsync(url);
     }
 }
