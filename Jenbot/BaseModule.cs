61a0af110a7ab3fa9e7f38c5850d6b2a86ae4182
jenboc <jenson@jensoncain.co.uk>
2025-01-20 02:47:03 +0000

    User Info Commands
    Added commands that gives the user links to my website, itch.io page and github page

--- Jenbot/BaseModule.cs
+++ Jenbot/BaseModule.cs
@@ -6,10 +6,41 @@ namespace Jenbot;
 
 public class BaseModule : ApplicationCommandModule
 {
+    private const string WEBSITE_URL = "https://www.jensoncain.co.uk";
+    private const string ITCH_URL = "https://jenboc.itch.io";
+    private const string GITHUB_URL = "https://github.com/jenboc";
+
     [SlashCommand("ping", "Ping the bot to ensure that it is running")]
     public async Task Ping(InteractionContext ctx)
     {
         await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource,
             new DiscordInteractionResponseBuilder().WithContent("Pong!")); 
+    }
+    
+    [SlashCommand("creator-website", "Get a link to the creator's website")]
+    public async Task CreatorWebsite(InteractionContext ctx)
+    {
+        var message = $"Check out Jenson's website here: {WEBSITE_URL}";
+
+        await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource,
+            new DiscordInteractionResponseBuilder().WithContent(message));
+    }
+
+    [SlashCommand("creator-itch", "Get a link to the creator's itch.io page")]
+    public async Task CreatorItch(InteractionContext ctx)
+    {
+        var message = $"Check out Jenson's itch.io page here: {ITCH_URL}";
+
+        await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource,
+            new DiscordInteractionResponseBuilder().WithContent(message));
+    }
+
+    [SlashCommand("creator-github", "Get a link to the creator's github page")]
+    public async Task CreatorGithub(InteractionContext ctx)
+    {
+        var message = $"Check out Jenson's github page here: {GITHUB_URL}";
+
+        await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource,
+            new DiscordInteractionResponseBuilder().WithContent(message));
     }
 }
